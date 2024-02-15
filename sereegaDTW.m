d1cb41c8862a82fd0411ab0f18c62718ac9192da
Tom Jefferis <10071295+tomjefferis@users.noreply.github.com>
2024-02-15 20:04:27 +0000

    Update sereegaDTW.m

--- sereegaDTW.m
+++ sereegaDTW.m
@@ -5,10 +5,9 @@ addpath funcs\
 
 %% Script config
 % script parameters
-n_permutations = 1000;
+n_permutations = 100;
 % Source parameters
-N_locations = 10; % number of potential source generators
-location_distance = 50; % minimum disrtance between generators mm
+location_distance = 5; % minimum disrtance between generators mm
 % Component parameters
 latency_difference = -0.05:0.01:0.05;
 n_components = 1:10; 
@@ -33,7 +32,7 @@ peak_lat_mse = zeros(length(n_components), length(sig_length), length(latency_di
 peak_area_mse = zeros(length(n_components), length(sig_length), length(latency_difference), n_permutations);
 
 
-for u = 1:length(n_components)
+parfor u = 1:length(n_components)
     n_component = n_components(u);
     
     temp_dtw_mse_median = zeros(length(sig_length), length(latency_difference), n_permutations);
@@ -48,71 +47,55 @@ for u = 1:length(n_components)
         sig_len = sig_length(i);
         for j = 1:length(latency_difference)
             latency_diff = latency_difference(j);
+
+
+            component_range = round(sig_len*fs*0.3) : round(sig_len*fs*0.8);
+                    
+            if sig_len*fs < 500
+                baselines = 10;
+            else
+                baselines = round(sig_len*fs*0.1);
+            end
+
+            sourcelocs  = lf_get_source_spaced(lf, n_permutations, location_distance);
+            amps = [min_amplitude:.1:min_amplitude/2, max_amplitude/2:.1:max_amplitude];
+            %erp = erp_get_class_random(n_component, component_range, component_widths, amps);
+            erp = erp_get_class_random(n_component, component_range, component_widths, amps,'numClasses', n_permutations);
+
+            % erp2 with added latency difference
+            erp2 = erp;
+            for k = 1:n_permutations
+                erp2(k).peakLatency = erp2(k).peakLatency + latency_diff*fs;
+            end
+
+            noise = struct( ...
+                    'type', 'noise', ...
+                    'color', 'pink', ...
+                    'amplitude', max(abs(min_amplitude),abs(max_amplitude))*SNR);
+            noise = utl_check_class(noise);
+
+            epochs = struct();
+            epochs.n = 1;             % the number of epochs to simulate
+            epochs.srate = fs;        % their sampling rate in Hz
+            epochs.length = sig_len*fs;       % their length in ms
+
             for k = 1:n_permutations
 
+
+                    dat1 = generate_signal_fromclass(erp(k), epochs);
+                    noise1 = generate_signal_fromclass(noise, epochs);
+                    data1 = dat1 + noise1;
                     
-                    component_range = round(sig_len*fs*0.3) : round(sig_len*fs*0.8);
                     
-                    if sig_len*fs < 500
-                        baselines = 10;
-                    else
-                        baselines = round(sig_len*fs*0.1);
-                    end
-                    
-                    sourcelocs  = lf_get_source_spaced(lf, N_locations, location_distance);
-                    amps = [min_amplitude:.1:min_amplitude/2, max_amplitude/2:.1:max_amplitude];
-                    erp = erp_get_class_random(n_component, component_range, component_widths, amps);
-
-                    % erp2 with added latency difference
-                    erp2 = erp;
-                    erp2.peakLatency = erp2.peakLatency + round(latency_diff*fs);
-
-                    noise = struct( ...
-                            'type', 'noise', ...
-                            'color', 'pink', ...
-                            'amplitude', max(abs(min_amplitude),abs(max_amplitude))*SNR);
-                    noise = utl_check_class(noise);
-
-                    epochs = struct();
-                    epochs.n = 1;             % the number of epochs to simulate
-                    epochs.srate = fs;        % their sampling rate in Hz
-                    epochs.length = sig_len*fs;       % their length in ms
-
-                    % combining into components
-                    c = struct();
-                    c.source = sourcelocs;      % obtained from the lead field, as above
-                    c.signal = {erp,noise};       % ERP class, defined above
-
-                    c = utl_check_component(c, lf);
-                    scalpdata = generate_scalpdata(c, lf, epochs);
-
-                    c = struct();
-                    c.source = sourcelocs;      % obtained from the lead field, as above
-                    c.signal = {erp2,noise};       % ERP class, defined above
-                    c = utl_check_component(c, lf);
-                    scalpdata2 = generate_scalpdata(c, lf, epochs);
-
-                    EEG1 = utl_create_eeglabdataset(scalpdata, epochs, lf);
-                    data1 = eeglab2fieldtrip(EEG1, 'timelock', 'none');
-
-                    EEG2 = utl_create_eeglabdataset(scalpdata2, epochs, lf);
-                    data2_t = eeglab2fieldtrip(EEG2, 'timelock', 'none');
-
-                    % check both signals are the same length... shouldnt
-                    % ever throw this in prod builds
-                    if size(data1.avg,2) ~= size(data2_t.avg,2)
-                        % throw error and stop
-                        error('Data is not the same length, check code.')
-                    end
-
-                    % get random electrode  
-                    electrode = randi([1,size(data1.avg,1)],1,1);
+                    dat2 = generate_signal_fromclass(erp2(k), epochs);
+                    noise2 = generate_signal_fromclass(noise, epochs);
+                    data2_t = dat2 + noise2;
 
                     
                     data = struct();
-                    data.erp = ft_preproc_bandpassfilter(data1.avg(electrode,:),fs,[1 30])';
+                    data.erp = ft_preproc_bandpassfilter(data1,fs,[1 30])';
                     data2 = struct();
-                    data2.erp = ft_preproc_bandpassfilter(data2_t.avg(electrode,:),fs,[1 30])';
+                    data2.erp = ft_preproc_bandpassfilter(data2_t,fs,[1 30])';
                     
                     
                     % determine latency difference
@@ -123,13 +106,13 @@ for u = 1:length(n_components)
                     baselineLat = baselineDeviation(data2,data,fs, baselines,2);
 
                     % get mse for each metric
-                    temp_dtw_mse_median(u,i,j,k) = mean((dtw_median - latency_diff).^2);
-                    temp_dtw_mse_weighted_median(u,i,j,k) = mean((dtw_weighted_median - latency_diff).^2);
-                    temp_dtw_mse_95(u,i,j,k) = mean((dtw_95 - latency_diff).^2);
-                    temp_baseline_mse(u,i,j,k) = mean((baselineLat - latency_diff).^2);
-                    temp_frac_peak_mse(u,i,j,k) = mean((fracPeakLat - latency_diff).^2);
-                    temp_peak_lat_mse(u,i,j,k) = mean((peakLat - latency_diff).^2);
-                    temp_peak_area_mse(u,i,j,k) = mean((areaLat - latency_diff).^2);
+                    temp_dtw_mse_median(i,j,k) = mean((dtw_median - latency_diff).^2);
+                    temp_dtw_mse_weighted_median(i,j,k) = mean((dtw_weighted_median - latency_diff).^2);
+                    temp_dtw_mse_95(i,j,k) = mean((dtw_95 - latency_diff).^2);
+                    temp_baseline_mse(i,j,k) = mean((baselineLat - latency_diff).^2);
+                    temp_frac_peak_mse(i,j,k) = mean((fracPeakLat - latency_diff).^2);
+                    temp_peak_lat_mse(i,j,k) = mean((peakLat - latency_diff).^2);
+                    temp_peak_area_mse(i,j,k) = mean((areaLat - latency_diff).^2);
                                        
                     
             end
