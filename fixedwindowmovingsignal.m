ffdc658c6a737eab80bc41d4fae2384e1eb2aa41
Tom Jefferis <10071295+tomjefferis@users.noreply.github.com>
2024-04-24 15:24:01 +0000

    Update fixedwindowmovingsignal.m

--- fixedwindowmovingsignal.m
+++ fixedwindowmovingsignal.m
@@ -28,16 +28,25 @@ end
 
 
 % result arrays
-dtw_mse_median = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-dtw_mse_weighted_median = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-dtw_mse_95 = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-baseline_mse = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-frac_peak_mse = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-peak_lat_mse = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
-peak_area_mse = NaN(length(SNRs),length(sig_length), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+dtw_mse_median = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+dtw_mse_weighted_median = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+dtw_mse_95 = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+baseline_mse = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+frac_peak_mse = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+peak_lat_mse = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+peak_area_mse = NaN(length(SNRs), length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
 
-for i = 1:length(SNRs)
+parfor i = 1:length(SNRs)
     SNR = SNRs(i);
+    
+    % temp arrays
+    temp_dtw_mse_median = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_dtw_mse_weighted_median = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_dtw_mse_95 = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_baseline_mse = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_frac_peak_mse = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_peak_lat_mse = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
+    temp_peak_area_mse = NaN( length(latency_difference), length(window_widths),length(allwinLocs), n_signals_generate);
     
     for j = 1:length(latency_difference)
         latency_diff = latency_difference(j);
@@ -71,25 +80,76 @@ for i = 1:length(SNRs)
                 
                 sig2 = generate_signal_fromclass(erp, epochs) + generate_signal_fromclass(noise, epochs);
                 
-
+                data = struct();
+                data.erp = ft_preproc_bandpassfilter(sig1,fs,[1 30])';
+                data2 = struct();
+                data2.erp = ft_preproc_bandpassfilter(sig2,fs,[1 30])';
+                
+                
                 window_location = 0:window_width/2:sig_length-window_width;
                 for l = 1:length(window_location)
                     window_start = window_location(l)*fs;
                     window_end = window_start + window_width*fs;
                     
-                    sig1_window = sig1(window_start:window_end);
-                    sig2_window = sig2(window_start:window_end);
+                    sig1_window = struct();
+                    sig2_window = struct();
+                    sig1_window.erp = data.erp(window_start:window_end);
+                    sig2_window.erp = data2.erp(window_start:window_end);
 
 
+                    [dtw_median, dtw_weighted_median, dtw_95] = dynamictimewarper(sig2_window, sig1_window, fs);
+                    peakLat = peaklatency(sig2_window,sig1_window,fs);
+                    fracPeakLat = fracpeaklatency(sig2_window,sig1_window,fs);
+                    areaLat = peakArea(sig2_window,sig1_window,fs, 0.5, baselines);
+                    baselineLat = baselineDeviation(sig2_window,sig1_window,fs, baselines,2);
+
+                    temp_dtw_mse_median(j,k,l,n) = mean((dtw_median - latency_diff).^2);
+                    temp_dtw_mse_weighted_median(j,k,l,n) = mean((dtw_weighted_median - latency_diff).^2);
+                    temp_dtw_mse_95(j,k,l,n) = mean((dtw_95 - latency_diff).^2);
+                    temp_baseline_mse(j,k,l,n) = mean((baselineLat - latency_diff).^2);
+                    temp_frac_peak_mse(j,k,l,n) = mean((fracPeakLat - latency_diff).^2);
+                    temp_peak_lat_mse(j,k,l,n) = mean((peakLat - latency_diff).^2);
+                    temp_peak_area_mse(j,k,l,n) = mean((areaLat - latency_diff).^2);
+                                       
+                    
                 end
             end
         end
     end
+    dtw_mse_median(i,:,:,:,:) = temp_dtw_mse_median;
+    dtw_mse_weighted_median(i,:,:,:,:) = temp_dtw_mse_weighted_median;
+    dtw_mse_95(i,:,:,:,:) = temp_dtw_mse_95;
+    baseline_mse(i,:,:,:,:) = temp_baseline_mse;
+    frac_peak_mse(i,:,:,:,:) = temp_frac_peak_mse;
+    peak_lat_mse(i,:,:,:,:) = temp_peak_lat_mse;
+    peak_area_mse(i,:,:,:,:) = temp_peak_area_mse;
 end
-    
-    %% Questions to ask Howard
-    % 1. How to implement the amplitude variability
-    % 2. How to implement the SNR
-    % 3. How to implement the windowing, should i test each size window in each location?
-    % 4. How to implement the jitter between components, if jitter too big would make component latency 0
-    
+
+% save results
+save('Results\ChangingWindow\dtw_mse_median.mat', 'dtw_mse_median')
+save('Results\ChangingWindow\dtw_mse_weighted_median.mat', 'dtw_mse_weighted_median')
+save('Results\ChangingWindow\dtw_mse_95.mat', 'dtw_mse_95')
+save('Results\ChangingWindow\baseline_mse.mat', 'baseline_mse')
+save('Results\ChangingWindow\frac_peak_mse.mat', 'frac_peak_mse')
+save('Results\ChangingWindow\peak_lat_mse.mat', 'peak_lat_mse')
+save('Results\ChangingWindow\peak_area_mse.mat', 'peak_area_mse')
+% save params for reference
+params = struct();
+params.n_signals_generate = n_signals_generate;
+params.latency_difference = latency_difference;
+params.SNRs = SNRs;
+params.n_components = n_components;
+params.component_widths = component_widths;
+params.component_amplitude = component_amplitude;
+params.fs = fs;
+params.sig_length = sig_length;
+params.intercomponent_jitter = intercomponent_jitter;
+params.intercomponent_amp = intercomponent_amp;
+params.amplitude_variability = amplitude_variability;
+params.window_widths = window_widths;
+params.allwinLocs = allwinLocs;
+
+save('Results\ChangingWindow\params.mat', 'params')
+
+
+
