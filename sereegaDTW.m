4e246c57aeb6086a4d258110246a245a89417c40
Tom Jefferis <10071295+tomjefferis@users.noreply.github.com>
2024-04-24 10:31:40 +0000

    Changing SNR implementation

--- sereegaDTW.m
+++ sereegaDTW.m
@@ -1,83 +1,87 @@
 %% adding paths
-addpath(genpath('SEREEGA-master\')) 
+addpath(genpath('SEREEGA-master\'))
 addpath(genpath('W:\PhD\MatlabPlugins\fieldtrip-20210906')); % path to fieldtrip
 addpath funcs\
 
 %% Script config
 % script parameters
-n_signals_generate = 10000; 
+n_signals_generate = 5000;
 % Component parameters
 latency_difference = -0.1:0.01:0.1;
-n_components = 1:8; 
+n_components = 1:8;
 component_widths = 25:250;
 min_amplitude = -10;
-max_amplitude = 10; 
-SNR = 0.3; % Signal to noise ratio, leaving at 0.3 for 'good looking' ERPs
+max_amplitude = 10;
+SNRs = 0.1:0.05:1; % Signal to noise ratio, leaving at 0.3 for 'good looking' ERPs
 fs = 1000; % sample rate
-sig_length = 0.3:0.1:3; % time in S
+sig_length = 0.2:0.05:1; % time in S
 amplitude_variability = 0.1; % variability of amplitude, not implemented yet
 
 
 % result arrays
-dtw_mse_median = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-dtw_mse_weighted_median = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-dtw_mse_95 = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-baseline_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-frac_peak_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-peak_lat_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
-peak_area_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+dtw_mse_median = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+dtw_mse_weighted_median = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+dtw_mse_95 = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+baseline_mse = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+frac_peak_mse = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+peak_lat_mse = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+peak_area_mse = ones(length(SNRs),length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
 
 
+parfor t = 1:length(SNRs)
+    SNR = SNRs(t);
 
-for u = 1:length(sig_length)
-    sig_len = sig_length(u);
     
-    temp_dtw_mse_median = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_dtw_mse_weighted_median = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_dtw_mse_95 = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_baseline_mse = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_frac_peak_mse = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_peak_lat_mse = ones(length(n_components), length(latency_difference), n_signals_generate);
-    temp_peak_area_mse = ones(length(n_components), length(latency_difference), n_signals_generate);
 
-    for i = 1:length(n_components)
-        n_component = n_components(i);
-        for j = 1:length(latency_difference)
-            latency_diff = latency_difference(j);
-
-
-            component_range = round(sig_len*fs*0.3) : round(sig_len*fs*0.8);
-                    
-            if sig_len*fs < 500
-                baselines = 10;
-            else
-                baselines = round(sig_len*fs*0.1);
-            end
-
-            amps = [min_amplitude:.1:min_amplitude/2, max_amplitude/2:.1:max_amplitude];
-            %erp = erp_get_class_random(n_component, component_range, component_widths, amps);
-            erp = erp_get_class_random(n_component, component_range, component_widths, amps,'numClasses', n_signals_generate);
-
-            % erp2 with added latency difference
-            erp2 = erp;
-            for k = 1:n_signals_generate
-                erp2(k).peakLatency = erp2(k).peakLatency + latency_diff*fs;
-            end
-
-            noise = struct( ...
+    temp_dtw_mse_median = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_dtw_mse_weighted_median = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_dtw_mse_95 = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_baseline_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_frac_peak_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_peak_lat_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    temp_peak_area_mse = ones(length(sig_length), length(n_components), length(latency_difference), n_signals_generate);
+    
+    for u = 1:length(sig_length)
+        sig_len = sig_length(u);
+        
+        for i = 1:length(n_components)
+            n_component = n_components(i);
+            for j = 1:length(latency_difference)
+                latency_diff = latency_difference(j);
+                
+                
+                component_range = round(sig_len*fs*0.3) : round(sig_len*fs*0.8);
+                
+                if sig_len*fs < 500
+                    baselines = 10;
+                else
+                    baselines = round(sig_len*fs*0.1);
+                end
+                
+                amps = [min_amplitude:.1:min_amplitude/2, max_amplitude/2:.1:max_amplitude];
+                %erp = erp_get_class_random(n_component, component_range, component_widths, amps);
+                erp = erp_get_class_random(n_component, component_range, component_widths, amps,'numClasses', n_signals_generate);
+                
+                % erp2 with added latency difference
+                erp2 = erp;
+                for k = 1:n_signals_generate
+                    erp2(k).peakLatency = erp2(k).peakLatency + latency_diff*fs;
+                end
+                
+                noise = struct( ...
                     'type', 'noise', ...
                     'color', 'pink', ...
                     'amplitude', max(abs(min_amplitude),abs(max_amplitude))*SNR);
-            noise = utl_check_class(noise);
-
-            epochs = struct();
-            epochs.n = 1;             % the number of epochs to simulate
-            epochs.srate = fs;        % their sampling rate in Hz
-            epochs.length = sig_len*fs;       % their length in ms
-
-            for k = 1:n_signals_generate
-
-
+                noise = utl_check_class(noise);
+                
+                epochs = struct();
+                epochs.n = 1;             % the number of epochs to simulate
+                epochs.srate = fs;        % their sampling rate in Hz
+                epochs.length = sig_len*fs;       % their length in ms
+                
+                for k = 1:n_signals_generate
+                    
+                    
                     dat1 = generate_signal_fromclass(erp(k), epochs);
                     noise1 = generate_signal_fromclass(noise, epochs);
                     data1 = dat1 + noise1;
@@ -86,7 +90,7 @@ for u = 1:length(sig_length)
                     dat2 = generate_signal_fromclass(erp2(k), epochs);
                     noise2 = generate_signal_fromclass(noise, epochs);
                     data2_t = dat2 + noise2;
-
+                    
                     
                     data = struct();
                     data.erp = ft_preproc_bandpassfilter(data1,fs,[1 30])';
@@ -100,33 +104,36 @@ for u = 1:length(sig_length)
                     fracPeakLat = fracpeaklatency(data2,data,fs);
                     areaLat = peakArea(data2,data,fs, 0.5, baselines);
                     baselineLat = baselineDeviation(data2,data,fs, baselines,2);
-
-                    % get mse for each metric
-                    temp_dtw_mse_median(i,j,k) = mean((dtw_median - latency_diff).^2);
-                    temp_dtw_mse_weighted_median(i,j,k) = mean((dtw_weighted_median - latency_diff).^2);
-                    temp_dtw_mse_95(i,j,k) = mean((dtw_95 - latency_diff).^2);
-                    temp_baseline_mse(i,j,k) = mean((baselineLat - latency_diff).^2);
-                    temp_frac_peak_mse(i,j,k) = mean((fracPeakLat - latency_diff).^2);
-                    temp_peak_lat_mse(i,j,k) = mean((peakLat - latency_diff).^2);
-                    temp_peak_area_mse(i,j,k) = mean((areaLat - latency_diff).^2);
-                                       
                     
+                    % get mse for each metric
+                    temp_dtw_mse_median(u,i,j,k) = mean((dtw_median - latency_diff).^2);
+                    temp_dtw_mse_weighted_median(u,i,j,k) = mean((dtw_weighted_median - latency_diff).^2);
+                    temp_dtw_mse_95(u,i,j,k) = mean((dtw_95 - latency_diff).^2);
+                    temp_baseline_mse(u,i,j,k) = mean((baselineLat - latency_diff).^2);
+                    temp_frac_peak_mse(u,i,j,k) = mean((fracPeakLat - latency_diff).^2);
+                    temp_peak_lat_mse(u,i,j,k) = mean((peakLat - latency_diff).^2);
+                    temp_peak_area_mse(u,i,j,k) = mean((areaLat - latency_diff).^2);
+                    
+                    
+                end
             end
         end
     end
-
-    dtw_mse_median(u,:,:,:) = temp_dtw_mse_median;
-    dtw_mse_weighted_median(u,:,:,:) = temp_dtw_mse_weighted_median;
-    dtw_mse_95(u,:,:,:) = temp_dtw_mse_95;
-    baseline_mse(u,:,:,:) = temp_baseline_mse;
-    frac_peak_mse(u,:,:,:) = temp_frac_peak_mse;
-    peak_lat_mse(u,:,:,:) = temp_peak_lat_mse;
-    peak_area_mse(u,:,:,:) = temp_peak_area_mse;
-    
+    dtw_mse_median(t,:,:,:,:) = temp_dtw_mse_median;
+    dtw_mse_weighted_median(t,:,:,:,:) = temp_dtw_mse_weighted_median;
+    dtw_mse_95(t,:,:,:,:) = temp_dtw_mse_95;
+    baseline_mse(t,:,:,:,:) = temp_baseline_mse;
+    frac_peak_mse(t,:,:,:,:) = temp_frac_peak_mse;
+    peak_lat_mse(t,:,:,:,:) = temp_peak_lat_mse;
+    peak_area_mse(t,:,:,:,:) = temp_peak_area_mse;
 end
 
+expParams = struct();
+expParams.components = n_components;
+expParams.latency_difference = latency_difference;
+expParams.sig_length = sig_length;
 
-
+save('Results\data\expParams.mat', 'expParams')
 save('Results\data\dtw_mse_median.mat', 'dtw_mse_median')
 save('Results\data\dtw_mse_weighted_median.mat', 'dtw_mse_weighted_median')
 save('Results\data\dtw_mse_95.mat', 'dtw_mse_95')
